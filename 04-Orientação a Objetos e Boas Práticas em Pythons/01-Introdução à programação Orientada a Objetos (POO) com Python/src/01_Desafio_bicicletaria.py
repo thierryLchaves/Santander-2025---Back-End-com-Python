c3e29a862c22962eab8fe0d20d58cb7ce7f90e1e
tlchaves <thierry.lucaschaves@gmail.com>
2025-07-02 17:26:16 -0300

    aula 1 modulos POO

---
+++ 04-Orientação a Objetos e Boas Práticas em Pythons/01-Introdução à programação Orientada a Objetos (POO) com Python/src/01_Desafio_bicicletaria.py
@@ -0,0 +1,15 @@
+class Bicicleta:
+    def __init__(self,cor, modelo, ano, valor):
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano
+        self.valor = valor
+
+    def buzinar(self):
+        print("bibi")
+    
+    def correr(self):
+        print("vruuuuuum")
+
+    def parar(self):
+        print("parando")
