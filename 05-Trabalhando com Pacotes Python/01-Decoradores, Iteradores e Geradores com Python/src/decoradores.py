5774561fb2f6c2118425b392b02463345660cde2
tlchaves <thierry.lucaschaves@gmail.com>
2025-07-10 16:49:38 -0300

    Aula 1 módulo 5

--- 05-Trabalhando com Pacotes Python/01-Decoradores, Iteradores e Geradores com Python/src/decoradores.py
+++ 05-Trabalhando com Pacotes Python/01-Decoradores, Iteradores e Geradores com Python/src/decoradores.py
@@ -1,14 +1,15 @@
-def meu_decorador(funcao):
-    def envelope():
-        print("Faz algo antes de executar a função ")
-        funcao()
-        print("Faz algo depois de executar a função ")
-    return envelope
+# def meu_decorador(funcao):
+#     def envelope():
+#         print("Faz algo antes de executar a função ")
+#         funcao()
+#         print("Faz algo depois de executar a função ")
+#     return envelope
 
-@meu_decorador
-def ola_mundo():
-    print("Olá mundo !")
+# @meu_decorador
+# def ola_mundo():
+#     print("Olá mundo !")
 
 
-# ola_mundo = meu_decorador(ola_mundo)
-ola_mundo()
+# # ola_mundo = meu_decorador(ola_mundo)
+# ola_mundo()
+
