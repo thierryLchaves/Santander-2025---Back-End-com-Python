64c437a486afadd435d40e851269687147b777e4
thierryLchaves <thierry.lucaschaves@gmail.com>
2025-07-26 13:10:35 -0300

    Changed container to Mariadb

--- 06-Integracao Python com Banco de dados/02-Introducao Banco de Dados Relacional (SQL)/db/DDL_Viagens_01.sql
+++ 06-Integracao Python com Banco de dados/02-Introducao Banco de Dados Relacional (SQL)/db/DDL_Viagens_01.sql
@@ -1,13 +1,21 @@
--- IF NOT EXISTS (SELECT * FROM sys.databases WHERE  name = 'viagens')
--- BEGIN
--- CREATE DATABASE viagens;
--- END
--- GO
--- USE viagens;
--- EXEC sp_addextendedproperty
---     @name = N'MS_Description',
---     @value = N'Descrição detalhada da tabela de clientes.',
---     @level0type = N'SCHEMA',
---     @level0name = N'dbo', -- ou o nome do seu schema
---     @level1type = N'TABLE',
---     @level1name = N'Clientes';
+CREATE TABLE viagens.usuarios (
+    id INT, 
+    nome VARCHAR(255) NOT NULL COMMENT 'Nome do usuário', 
+    email VARCHAR(100) NOT NULL UNIQUE COMMENT 'E-mail do usuário', 
+    endereco VARCHAR(50) NOT NULL COMMENT 'Endereço do usuário', 
+    data_nascimento DATE NOT NULL COMMENT 'Data de nascimento do usuário'
+); 
+
+CREATE TABle viagens.destinos(
+    id INT, 
+    nome VARCHAR(255) NOT NULL UNIQUE COMMENT 'Nome do destino', 
+    descricao VARCHAR(255) NOT NULL COMMENT 'Descrição do destino'
+);
+
+CREATE TABLE viagens.reservas(
+    id INT COMMENT 'Identificado único da reserva', 
+    id_usuario INT COMMENT 'Referência ao  ID do usuário que fez a reserva', 
+    id_destino INT COMMENT 'Referência ao ID  do usuário que fez a reserva', 
+    data DATE COMMENT 'Data da reserva', 
+    status VARCHAR(255) DEFAULT 'pendente' COMMENT 'Status da reserva (confirmada, pendente, cancelada, etc. )'
+);
